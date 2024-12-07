3624103d7c2f5efa9667488e65df84a963038a84
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-07 10:27:55 -0800

    bump server version

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// v0.0.1
+// v0.0.2
 
 const app = express();
 const port = process.env.PORT || 3000;
