c486fa738f9e8242731eaa1779d78c30cd818b7d
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-07 19:48:19 -0800

    test circleci config to run server CI

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// v0.0.11 -- used to trigger circleci for debug/testing
+// v0.0.12 -- used to trigger circleci for debug/testing
 
 const app = express();
 const port = process.env.PORT || 3000;
