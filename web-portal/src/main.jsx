1ffd5ed0cbed2c54f7d034bcac75cfedbc575c77
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-07 19:51:28 -0800

    test circleci config to run server CI

--- web-portal/src/main.jsx
+++ web-portal/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// v0.0.6 - used for debug/testing circleci
+// v0.0.7 - used for debug/testing circleci
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
