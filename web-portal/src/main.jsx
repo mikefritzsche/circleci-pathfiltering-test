6eaaf34c780ebbf18e3d06a72c102b0cab227eaf
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-08 09:11:06 -0800

    debug ssh copy process

--- web-portal/src/main.jsx
+++ web-portal/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// v0.0.8 - used for debug/testing circleci
+// v0.0.9 - used for debug/testing circleci
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
