71fe305c9eceed2a00477f2ef7b04dbce220157e
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-08 10:54:40 -0800

    update node version

--- web-portal/src/main.jsx
+++ web-portal/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// v0.0.10 - used for debug/testing circleci
+// v0.0.11 - used for debug/testing circleci
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
