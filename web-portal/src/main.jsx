b57ad071a8bec3f22b83dde43a0163def993fb0a
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-09 10:13:39 -0800

    revert config path parsing

--- web-portal/src/main.jsx
+++ web-portal/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// v0.0.20- used for debug/testing circleci
+// v0.0.21- used for debug/testing circleci
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
